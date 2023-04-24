4eca0f5acf20fdeffa97a1df846f2ea1dade6ee9
taillon miguel <taillonmiguel@live.com>
2023-04-24 19:37:51 -0300

    projeto web reformulado

---
+++ WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,133 @@
+﻿using CleanArch.AppService.DTOs;
+using CleanArch.AppService.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data;
+
+namespace WebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _environment;
+        public ProductsController(
+            IProductService productService,
+            ICategoryService categoryService,
+            IWebHostEnvironment environment)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+            _environment = environment;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetProducts();
+            return View(products);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId = new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO productDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Add(productDTO);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                ViewBag.CategoryId = new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+            }
+
+            return View(productDTO);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id is null)
+                return NotFound();
+
+            var productDto = await _productService.GetById(id);
+
+            if (productDto is null)
+                return NotFound();
+
+            var categories = await _categoryService.GetCategories();
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", productDto.CategoryId);
+
+            return View(productDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductDTO productDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _productService.Update(productDTO);
+                    return RedirectToAction("Index");
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return View(productDTO);
+        }
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id is null)
+                return NotFound();
+
+            var product = await _productService.GetById(id);
+
+            if (product is null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            await _productService.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id is null)
+                return NotFound();
+
+            var productDto = await _productService.GetById(id);
+
+            if (productDto is null)
+                return NotFound();
+
+            var wwroot = _environment.WebRootPath;
+            var image = Path.Combine(wwroot, "image\\" + productDto.Image);
+
+            var exists = System.IO.File.Exists(image);
+
+            ViewBag.ImageExisti = exists;
+
+            return View(productDto);
+        }
+    }
+}
---
+++ WebApp/Program.cs
@@ -0,0 +1,48 @@
+using CleanArch.Dependency;
+using CleanArch.Dominio.Account;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+SeedUserRoles(app);
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+void SeedUserRoles(IApplicationBuilder app)
+{
+    using (var serviceScope = app.ApplicationServices.CreateScope())
+    {
+        var seed = serviceScope.ServiceProvider
+                               .GetService<ISeedUserRoleInitial>();
+        seed.SeedUsers();
+        seed.SeRoles();
+    }
+}
